a20efc1db531344d6dac8991981f1a5b9aadfd3a
Kaleb Bolack <kalebbolack14@gmail.com>
2023-11-09 06:59:01 -0700

    Added Plant spawn zones and gave peashooter & sunflower card sun values!

--- lib/game.py
+++ lib/game.py
@@ -20,6 +20,59 @@ def load_sun_images():
     sun_images = [pygame.image.load(f"lib/Game/Sun Animation/sun-{i}.gif") for i in range(3, 23)]
     return sun_images
 
+# Define the plant spawn points (you can adjust these coordinates)
+plant_spawn_points = [
+    pygame.Rect(185, 115, 50, 50), pygame.Rect(265, 115, 50, 50), pygame.Rect(345, 115, 50, 50), pygame.Rect(425, 115, 50, 50), pygame.Rect(505, 115, 50, 50),
+    pygame.Rect(185, 200, 50, 50), pygame.Rect(265, 200, 50, 50), pygame.Rect(345, 200, 50, 50), pygame.Rect(425, 200, 50, 50), pygame.Rect(505, 200, 50, 50),
+    pygame.Rect(185, 300, 50, 50), pygame.Rect(265, 300, 50, 50), pygame.Rect(345, 300, 50, 50), pygame.Rect(425, 300, 50, 50), pygame.Rect(505, 300, 50, 50),
+    pygame.Rect(185, 400, 50, 50), pygame.Rect(265, 400, 50, 50), pygame.Rect(345, 400, 50, 50), pygame.Rect(425, 400, 50, 50), pygame.Rect(505, 400, 50, 50),
+    pygame.Rect(185, 500, 50, 50), pygame.Rect(265, 500, 50, 50), pygame.Rect(345, 500, 50, 50), pygame.Rect(425, 500, 50, 50), pygame.Rect(505, 500, 50, 50),
+    pygame.Rect(585, 115, 50, 50), pygame.Rect(665, 115, 50, 50), pygame.Rect(745, 115, 50, 50), pygame.Rect(825, 115, 50, 50),
+    pygame.Rect(585, 200, 50, 50), pygame.Rect(665, 200, 50, 50), pygame.Rect(745, 200, 50, 50), pygame.Rect(825, 200, 50, 50),
+    pygame.Rect(585, 300, 50, 50), pygame.Rect(665, 300, 50, 50), pygame.Rect(745, 300, 50, 50), pygame.Rect(825, 300, 50, 50),
+    pygame.Rect(585, 400, 50, 50), pygame.Rect(665, 400, 50, 50), pygame.Rect(745, 400, 50, 50), pygame.Rect(825, 400, 50, 50),
+    pygame.Rect(585, 500, 50, 50), pygame.Rect(665, 500, 50, 50), pygame.Rect(745, 500, 50, 50), pygame.Rect(825, 500, 50, 50),
+]
+class SelectedPlant:
+    def __init__(self, plant):
+        self.plant = plant
+        self.image = plant.card_image
+        self.rect = self.image.get_rect()
+        self.cost = plant.cost
+        self.dragging = False
+
+    def update_position(self, x, y):
+        self.rect.topleft = (x, y)
+
+def select_plant(plant):
+    global selected_plant
+    selected_plant = SelectedPlant(plant)
+
+class Plant:
+    def __init__(self, name, cost, card_image):
+        self.name = name
+        self.cost = cost
+        self.card_image = card_image
+        self.rect = None
+
+# Load plant card images
+peashooter_card_image = pygame.image.load("lib/Game/Peashooter-Card.png")
+sunflower_card_image = pygame.image.load("lib/Game/Sunflower-Card.png")
+
+# Define a list of available plant types
+available_plants = [
+    Plant("Peashooter", 100, peashooter_card_image),
+    Plant("Sunflower", 50, sunflower_card_image),
+    # Add more plant types as needed
+]
+
+selected_plant = None
+
+# Create a function to handle plant selection
+def select_plant(plant):
+    global selected_plant
+    selected_plant = plant
+
 def init_game():
     pygame.init()
     
@@ -52,8 +105,6 @@ def init_game():
     desired_card_height = 88
     peashooter_card_image = pygame.transform.scale(peashooter_card_image, (desired_card_width, desired_card_height))
 
-    print(f"Sunflower card image size: {sunflower_card_image.get_size()}")
-
     sun_images = load_sun_images()
     sun_instances = []
     current_frame = 0
@@ -71,10 +122,45 @@ def run_game():
     y = 0
     frame_delay = 55
 
+    selected_plant = None
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if peashooter_card_rect.collidepoint(event.pos):
+                    selected_plant = available_plants[0]
+                    if selected_plant:
+                        selected_plant.rect = selected_plant.card_image.get_rect()
+                        if isinstance(selected_plant, SelectedPlant):
+                            selected_plant.dragging = True
+                            mouse_x, mouse_y = event.pos
+                            selected_plant.rect.topleft = (mouse_x - selected_plant.rect.width // 2, mouse_y - selected_plant.rect.height // 2)
+                elif sunflower_card_rect.collidepoint(event.pos):
+                    selected_plant = available_plants[1]
+                    if selected_plant:
+                        selected_plant.rect = selected_plant.card_image.get_rect()
+                        if isinstance(selected_plant, SelectedPlant):
+                            selected_plant.dragging = True
+                            mouse_x, mouse_y = event.pos
+                            selected_plant.rect.topleft = (mouse_x - selected_plant.rect.width // 2, mouse_y - selected_plant.rect.height // 2)
+            elif event.type == pygame.MOUSEMOTION:
+                if selected_plant and isinstance(selected_plant, SelectedPlant) and selected_plant.dragging:
+                    mouse_x, mouse_y = event.pos
+                    selected_plant.update_position(mouse_x - selected_plant.rect.width // 2, mouse_y - selected_plant.rect.height // 2)
+
+        # Now, outside the event loop (but still inside the game loop), check for plant placement:
+        if selected_plant:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            for spawn_rect in plant_spawn_points:
+                if spawn_rect.collidepoint(mouse_x, mouse_y):
+                    if selected_plant.cost <= sun_count:
+                        sun_count -= selected_plant.cost
+                        selected_plant.rect.topleft = spawn_rect.topleft
+                        if hasattr(selected_plant, 'dragging'):
+                            selected_plant.dragging = False
+                        selected_plant = None
 
         current_time = pygame.time.get_ticks()
 
